3093074bebdfbfb684895cd2c55c6028f8f78a39
morgan-ashley <morgan.angela.ashley@gmail.com>
2016-02-10 18:13:12 -0800

    update
    added model and collection

--- js/app.js
+++ js/app.js
@@ -1,28 +1,66 @@
 (function($){
 
+	//The model acts as our JS object
+	var Words = Backbone.Model.extend({
+		defaults: {
+			word1: 'hello',
+			word2: 'there!'
+		},
+	});
+
+	//A collection is an array of models
+	var WordList = Backbone.Collection.extend({
+		model: Words
+	});
+
+	//The main view for our application
 	var AppView = Backbone.View.extend({
 		el: $('body'),
 
+		//Where DOM events are bound to the View methods
 		events: {
-			'click button#add': 'addItem'
+			'click button#add': 'addHello'
 		},
 
+		//where binding happen, excluding events
 		initialize: function(){
-			_.bindAll(this, 'render', 'addItem');
+			_.bindAll(this, 'render', 'addHello', 'appendHellos');
+
+			this.collection = new WordList();
+			this.collection.bind('add', this.appendHellos) //collection event binder
 			this.counter = 0;
 			this.render();
 		},
 
+		//Renders the view
 		render: function(){
-			$(this.el).append("<button id='add'>Add a hello</button>");
+
+			var self = this;
+			$(this.el).append("<button id='add'>Add a Hello</button>");
 			$(this.el).append("<ul></ul>");
+			_(this.collection.models).each(function(word){
+				self.appendHellos(word);
+			}, this);
 		},
 
-		addItem : function(){
+		//addHello() deals only with the model/collections
+		//View updates are delegated to the 'add' event listener appendHellos() below
+		addHello: function(){
 			this.counter++;
-			$('ul', this.el).append("<li>Hello there! " + this.counter+"</li>");
-		}
-	});
+			var words = new Words();
+			words.set({
+				word2: words.get('word2') + this.counter
+			});
 
+			this.collection.add(words);
+		},
+ 		
+ 		//Triggeres by collection event 'add', and handles visual update
+ 		appendHellos: function(words){
+      		$('ul', this.el).append("<li>"+words.get('word1')+" "+words.get('word2')+"</li>");
+    }
+  });
+			
+//instantiate the main applicaion view
 var appView = new AppView();
 })(jQuery)
